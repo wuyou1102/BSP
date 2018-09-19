ffe9653acb5085ebeed9ad5cfe535d624c31f119
wuyou1102 <jotey@qq.com>
2018-09-19 14:47:31 +0800

    add

--- BSP/urls.py
+++ BSP/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+import DailyVersion
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', DailyVersion.view.hello),
 ]
