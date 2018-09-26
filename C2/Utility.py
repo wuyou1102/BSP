0efb9ab63f1e7d5c5864298c19741e2d8aaaf6f6
wuyou1102 <jotey@qq.com>
2018-09-26 16:43:12 +0800

    add

--- C2/Utility.py
+++ C2/Utility.py
@@ -55,7 +55,7 @@ def format_commit_msg(lines):
                     commit_attrs.append([v, line.lstrip(k)])
                     block.remove(line)
                     break
-        commit["msg"] = "\n".join(block)
+        commit["msg"] = "<br>".join(block)
         commit["attr"] = commit_attrs
         lst.append(commit)
     return lst
