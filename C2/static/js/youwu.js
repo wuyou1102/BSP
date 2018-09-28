d47e2f73c1ad10c9c38b57a60805bb66080651eb
wuyou1102 <jotey@qq.com>
2018-09-28 16:32:20 +0800

    ad

--- C2/static/js/youwu.js
+++ C2/static/js/youwu.js
@@ -93,7 +93,7 @@ function CreateUploadButton(Data,Type){
     var Input =document.createElement("input");
     Input.type="file";
     Input.className="upload uploadBtn";
-    Input.addEventListener("change",AjaxUpload(this));
+    Input.addEventListener("change","AjaxUpload(this)");
     Div.appendChild(Span);
     Div.appendChild(Input);
     return Div;
@@ -109,5 +109,5 @@ function CreateReleaseNotes(ReleaseNotes){
 }
 
 function AjaxUpload(obj){
-    alert(this.type)
+    alert(this.type )
 }
