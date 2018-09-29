948dfed6b7680695065497235db594adf009d945
wuyou1102 <jotey@qq.com>
2018-09-29 14:25:59 +0800

    add

--- C2/CommonRequest/view.py
+++ C2/CommonRequest/view.py
@@ -18,10 +18,7 @@ def commit_history(request):
         relative_path = request.GET["path"]
         _type = request.GET["type"]
         abs_path = os.path.join(Path.get_path(_type), relative_path)
-        # context['name'] = __get_commit_history_name(relative_path)
-        name="王庆军，请教大王"
-        print repr(name)
-        context['name'] = repr(name)
+        context['name'] = __get_commit_history_name(relative_path)
         context['commits'] = __parse_commits(abs_path)
         return render(request, 'CommitHistory.html', context)
     else:
