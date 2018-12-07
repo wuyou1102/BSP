7097f79627c63e3dd84de541fbb3d28610659233
wuyou1102 <jotey@qq.com>
2018-12-07 22:12:42 +0800

    add

--- C2/B2_DailyVersion/view.py
+++ C2/B2_DailyVersion/view.py
@@ -18,22 +18,33 @@ def __get_daily_build_info():
     for build in builds:
         dict_build = dict()
         build_path = os.path.join(PATH_DAILY, build)
+        version = os.path.join(build_path, 'VersionNumber.txt')
         commit_history = os.path.join(build_path, 'CommitHistory.txt')
         dict_build['name'] = build
-        dict_build['binaries'] = __get_binary(build_path)
+        dict_build['version'] = __get_version_number(version)
+        dict_build['images'] = __get_images(build_path)
         dict_build['commit_history'] = __get_commit_history(commit_history)
+
         lst.append(dict_build)
     return sorted(lst, key=lambda k: k['name'], reverse=True)
 
 
-def __get_binary(path):
+def __get_version_number(path):
+    if os.path.exists(path):
+        with open(path, 'r') as f:
+            return f.read().strip('\r\n')
+    else:
+        return ''
+
+
+def __get_images(path):
     lst = list()
     if not os.path.exists(path):
         return lst
     for f in os.listdir(path):
-        if f.endswith('.img'):
+        if f.endswith('.zip'):
             file_path = os.path.join(path, f).replace(PATH_DAILY, '')
-            file_name = f.rstrip('.img')
+            file_name = f.rstrip('.zip')
             _file = [file_name, file_path]
             lst.append(_file)
     return sorted(lst, key=lambda k: k[0], reverse=False)
