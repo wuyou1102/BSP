ce03ad76c739ed7def0e9a1b717fec0b00c9406e
wuyou1102 <jotey@qq.com>
2018-12-10 21:53:26 +0800

    add

--- Server/urls.py
+++ Server/urls.py
@@ -1,4 +1,4 @@
-"""C2 URL Configuration
+"""Server URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
