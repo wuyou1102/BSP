cf83321be07609f1a5628a8b6f02d1cdb7860765
wuyou1102 <jotey@qq.com>
2018-12-10 21:21:43 +0800

    add

--- C2/B2_DailyVersion/view.py
+++ C2/B2_DailyVersion/view.py
@@ -3,22 +3,24 @@ from django.shortcuts import render
 import os
 from C2.Utility import Path
 
-PATH_DAILY = Path.B2_DailyBuild
+PATH_9A_DAILY = Path.B2_DailyBuild
+PATH_9B_DAILY = Path.B2_DailyBuild
 
 
 def get_9A_build_info(request):
     context = dict()
-    context['builds'] = __get_daily_build_info()
+    context['builds'] = __get_daily_build_info(PATH_9A_DAILY)
     return render(request, 'B2_9A_DailyVersion.html', context)
 
 
 def get_9B_build_info(request):
     context = dict()
-    context['builds'] = __get_daily_build_info()
+    context['builds'] = __get_daily_build_info(PATH_9B_DAILY)
     return render(request, 'B2_9B_DailyVersion.html', context)
 
 
-def __get_daily_build_info():
+def __get_daily_build_info(path):
+    PATH_DAILY = path
     lst = []
     builds = os.listdir(PATH_DAILY)
     for build in builds:
--- C2/Utility/Path.py
+++ C2/Utility/Path.py
@@ -1,13 +1,15 @@
 C2_DailyBuild = "/home/bspserver/sda/C2_DailyBuild/"
 C2_WeeklyBuild = "/home/bspserver/sda/C2_WeeklyBuild/"
 C2_TriggerBuild = "/home/bspserver/sda/C2_TriggerBuild/"
-B2_DailyBuild = "/home/bspserver/sda/B2_DailyBuild/"
+B2_9A_DailyBuild = "/home/bspserver/sda/B2_9A_DailyBuild/"
+B2_9B_DailyBuild = "/home/bspserver/sda/B2_9B_DailyBuild/"
 
 __switch = {
     "c2daily": C2_DailyBuild,
     "c2weekly": C2_WeeklyBuild,
     "c2trigger": C2_TriggerBuild,
-    "b2daily": B2_DailyBuild
+    "b29adaily": B2_9A_DailyBuild,
+    "b29bdaily": B2_9B_DailyBuild,
 }
 
 
